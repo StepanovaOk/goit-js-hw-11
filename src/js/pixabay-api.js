6fcac778471c9894f27bf20f049b6155b0b098ab
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2024-02-11 16:07:27 +0200

    second part

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,19 +3,22 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 const searchInput = document.querySelector('.input-name');
-const searchQuery = searchInput.value.trim();
+
+const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', getPhoto);
 
-if (searchQuery === '') {
-  iziToast.error({
-    title: 'Error',
-    message: 'Please enter a search query',
-  });
-}
-
 function getPhoto(event) {
   event.preventDefault();
+
+  const searchQuery = searchInput.value.trim();
+
+  if (searchQuery === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search query',
+    });
+  }
 
   const BASE_URL = 'https://pixabay.com';
   const END_POINT = '/api/';
@@ -30,5 +33,47 @@ function getPhoto(event) {
 
   const url = `${BASE_URL}${END_POINT}?${params}`;
 
-  fetch(url).then(res);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => renderPhotos(data.hits))
+    .catch(error => console.log('Error fetching data:', error));
+}
+
+function makeMarkup(webformatURL, tags, likes, views, comments, downloads) {
+  return `<li class="photo">
+    <div class="card-image">
+      <img src="${webformatURL}" alt=${tags}></img>
+      <div class="description">
+        <p> Likes ${likes}</p>
+        <p> Views ${views}</p>
+        <p> Comments ${comments}</p>
+        <p> Downloads ${downloads}</p>
+      </div>
+    </div>
+  </li>`;
+}
+
+function renderPhotos(photo) {
+  gallery.innerHTML = '';
+
+  photo.forEach(photo => {
+    const {
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    } = photo;
+    const photoElement = makeMarkup(
+      webformatURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads
+    );
+    gallery.insertAdjacentHTML('beforeend', photoElement);
+  });
 }
